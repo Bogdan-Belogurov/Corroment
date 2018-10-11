2dfaa46f0cb9c0c937d2906705b7548f939bd6fc
Bogdan Belogurov <belbog@me.com>
2018-10-11 18:30:31 +0300

    Initial

--- Anastasiya/ViewController.swift
+++ Anastasiya/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calculateButton.layer.cornerRadius = 20
+        calculateButton.layer.borderWidth = 1.0
+        calculateButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        calculateButton.clipsToBounds = true
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
 

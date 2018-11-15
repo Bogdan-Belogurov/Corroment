e5ab76521d6aa97d40457f4f757e36ef90d41cc5
Bogdan Belogurov <belbog@me.com>
2018-11-15 18:44:58 +0300

    Create intro animations

---
+++ Anastasiya/IntroAnimationViewController.swift
@@ -0,0 +1,77 @@
+//
+//  IntroAnimationViewController.swift
+//  Anastasiya
+//
+//  Created by Bogdan Belogurov on 15/11/2018.
+//  Copyright © 2018 Bogdan Belogurov. All rights reserved.
+//
+
+import UIKit
+
+class IntroAnimationViewController: UIViewController {
+    
+    @IBOutlet weak var introLabel: UILabel!
+    @IBOutlet weak var introLabelYAnchor: NSLayoutConstraint!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        continueButton.layer.cornerRadius = 12
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.textLabel.alpha = 0
+        self.continueButton.alpha = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            UIView.animate(withDuration: 1, animations: {
+                self.introLabelYAnchor.constant = 30
+                self.view.layoutIfNeeded()
+            })
+            UIView.animate(withDuration: 1, delay: 0.5, animations: {
+                self.textLabel.alpha = 1
+                self.continueButton.alpha = 1
+            })
+        }
+    }
+    @IBAction func closeButton(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(true, forKey: "wasIntroWatched")
+        userDefaults.synchronize()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+     import UIKit
+     
+     class IntroViewController: UIViewController {
+     
+     
+     override func viewDidAppear(_ animated: Bool) {
+     super.viewDidAppear(true)
+     DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+     UIView.animate(withDuration: 1, animations: {
+     self.introViewYAnchor.constant = -150
+     self.view.layoutIfNeeded()
+     })
+     UIView.animate(withDuration: 1, delay: 0.5, animations: {
+     self.textView.alpha = 1
+     self.continueButtonOutlet.alpha = 1
+     })
+     }
+     }
+     
+     @IBAction func continueButtonTap(_ sender: Any) {
+     }
+     
+     }
+    */
+
+}
--- Anastasiya/ViewController.swift
+++ Anastasiya/ViewController.swift
@@ -14,14 +14,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setCalculateButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.showIntroContoller()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func setCalculateButton() {
         calculateButton.layer.cornerRadius = 20
         calculateButton.layer.borderWidth = 1.0
         calculateButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         calculateButton.clipsToBounds = true
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    
+    func showIntroContoller()  {
+        let userDefaults = UserDefaults.standard
+        let wasIntroWatched = userDefaults.bool(forKey: "wasIntroWatched")
+        guard !wasIntroWatched else {return}
+        if let pageViewController = storyboard?.instantiateViewController(withIdentifier: "introAnimationViewController") as? IntroAnimationViewController {
+            present(pageViewController, animated: true, completion: nil)
+        }
     }
 
+
 }
+
+
 

a2aba9ebb6e67f703971456153d6a8bddfa74701
Bogdan Belogurov <belbog@me.com>
2019-05-03 16:07:07 +0300

    Chart fix

---
+++ Anastasiya/PresentationLayer/Main/Cell/InputParametersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ParametersTableViewCell.swift
+//  Anastasiya
+//
+//  Created by Bogdan Belogurov on 01/05/2019.
+//  Copyright © 2019 Bogdan Belogurov. All rights reserved.
+//
+
+import UIKit
+
+class ParametersTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
---
+++ Anastasiya/PresentationLayer/Models/ParametersModel.swift
@@ -0,0 +1,9 @@
+//
+//  ParametersModel.swift
+//  Anastasiya
+//
+//  Created by Bogdan Belogurov on 01/05/2019.
+//  Copyright © 2019 Bogdan Belogurov. All rights reserved.
+//
+
+import Foundation
